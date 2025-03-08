bfcb9418f6dece63987fa6aa00decf9112e5b833
Pooja-Priyanka <priya.priyankagadugoyyala.67@gmail.com>
2025-03-08 11:57:17 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,19 @@
+import java.util.scanner;
+
+class Main{
+	public static void main(Strings[] args){
+		
+		int age;
+		Scanner input = new scanner(System.in);
+		System.out.print("enter age");
+		age = input.nextDouble();
+		System.out.println("miinor")
+		if (age>18){
+			System.out.println("student is major");
+		}
+		else{
+			System.out.println("student is minor");
+		}
+	}
+}
+		
