d1cfc2905ce04688bb55b2fc15c8bb698e151555
Pooja-Priyanka <priya.priyankagadugoyyala.67@gmail.com>
2025-03-12 01:29:43 +0530

    Add files via upload

--- book.java
+++ book.java
@@ -1,29 +1,31 @@
-public class book{
-	//attributes
-	String title;
-	String author;
-	int year_of_publication;
-	//constructor to initialize book details
-	book(String title, String author, int year_of_publication){
-	this.title=title;
-	this.author=author;
-	this.year_of_publication=year_of_publication;
-	System.out.println("title of the book:" +title);
-	System.out.println("author name:" +author);
-	System.out.println("year of publication:" +year_of_publication);
-	}
-	public void Display(){
-	System.out.println("title of the book:" +title);
-	System.out.println("author name:" +author);
-	System.out.println("year of publication:" +year_of_publication);
-	}
- 
-     public static void main(String[] args) {
-         // Creating c1
-         book c1= new book("harry potter1","J.K.rowling",2007);
+public class book {
+    // Attributes
+    String title;
+    String author;
+    int year_of_publication;
 
-         // Creating c2
-         book c2 = new book("harry potter2","J.K.rowling",2008);
+    // Constructor to initialize book details
+    book(String title, String author, int year_of_publication) {
+        this.title = title;
+        this.author = author;
+        this.year_of_publication = year_of_publication;
+    }
 
-     }
+    // Method to display book details
+    void display() {
+        System.out.println("Title of the book: " + title);
+        System.out.println("Author name: " + author);
+        System.out.println("Year of publication: " + year_of_publication);
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        // Creating c1
+        book c1 = new book("Harry Potter 1", "J.K. Rowling", 2007);
+        c1.display();
+
+        // Creating c2
+        book c2 = new book("Harry Potter 2", "J.K. Rowling", 2008);
+        c2.display();
+    }
 }
